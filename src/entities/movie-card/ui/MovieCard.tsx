c68da2549b063442433f020112046c2e5c35ea3c
Margo <m.yunanova@gmail.com>
2024-06-14 20:10:52 +0300

    feat: add MovieCard component

---
+++ src/entities/movie-card/ui/MovieCard.tsx
@@ -0,0 +1,46 @@
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Chip, Stack } from "@mui/material";
+import { FC } from "react";
+
+interface IMovieCard {
+  name: string;
+  year: number;
+  rating: number;
+  poster: string;
+  genres: Record<"name", string>[];
+}
+
+export const MovieCard: FC<IMovieCard> = ({ name, year, rating, poster, genres }) => {
+  // TODO разобраться с высотой и растягиванием карточки
+  return (
+    <Card elevation={5}>
+      <CardActionArea>
+        <CardMedia component="img" alt={`Постер фильма ${name}`} src={poster} />
+        <CardContent>
+          <Stack
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            gap="5px"
+          >
+            <Stack display="flex" flexDirection="column">
+              <Typography variant="body1" color="text.primary">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {year}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {genres.map((item) => item.name).join(", ")}
+              </Typography>
+            </Stack>
+            <Chip label={rating} color="success" variant="outlined" />
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
